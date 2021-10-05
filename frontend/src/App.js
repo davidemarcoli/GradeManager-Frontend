1d79eef4709e30a20b50df10aff842d87e857eda
Davide Marcoli <davide.marcoli@noseryoung.ch>
2021-10-05 10:41:01 +0200

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {AddGrade} from "./AddGrade";
+
+function App() {
+  return (
+      <AddGrade></AddGrade>
+  );
+}
+
+export default App;
