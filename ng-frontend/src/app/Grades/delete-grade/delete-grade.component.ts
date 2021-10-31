4b72df0f6202640b9c9b82c74878ffd21a348b52
lazarpe <lazar.petrovic.nefti@gmail.com>
2021-10-31 18:59:22 +0100

    new state

--- ng-frontend/src/app/Grades/delete-grade/delete-grade.component.ts
+++ ng-frontend/src/app/Grades/delete-grade/delete-grade.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {Grade} from "../../model/Grade";
+import {User} from "../../model/User";
+import {Googleuser} from "../../global/googleuser";
+import {AppSettings} from "../../app.settings";
 
 @Component({
   selector: 'app-delete-grade',
   templateUrl: './delete-grade.component.html',
   styleUrls: ['./delete-grade.component.css']
 })
+
+@Injectable()
 export class DeleteGradeComponent implements OnInit {
 
-  constructor() { }
+  grades : Grade[] | undefined
+  id: number | undefined
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    const options = {headers: {'Content-Type': 'application/json'}};
+    this.http.get<any>(AppSettings.BASE_URL + 'grades/getbyuser/' + Googleuser.user.id).subscribe(data => {
+      this.grades = data;
+      console.log(data);
+    })
+  }
+
+  async onDropdownChange(event: any) {
+    this.id = event.target.value;
+    console.log("ID: " + this.id);
+  }
+
+  onSubmit(): void {
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
+      }
+    };
+
+    // this.http.post<Grade>('https://grade-manager.herokuapp.com/grades/', {
+    //   grade: this.form.controls.grade.value,
+    //   theme: this.form.controls.theme.value,
+    //   user: new User(this.form.controls.user.value.id, this.form.controls.theme.value.username, this.form.controls.theme.value.password),
+    // }, options).subscribe(data => console.log(data));
+
+    this.http.delete<any>(AppSettings.BASE_URL + 'grades/' + this.id);
+  }
 }
